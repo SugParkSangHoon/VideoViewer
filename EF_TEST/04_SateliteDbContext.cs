4ffff4481dec39b28194e9ef4a998b08c1d99a7a
DESKTOP-L8PQMEN\User <power93266@naver.com>
2023-01-12 00:21:08 +0900

    1. msSql 연결 실패...

---
+++ EF_TEST/04_SateliteDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_TEST
+{
+    public class SateliteDbContext : DbContext
+    {
+        ///PM> Install-Package Microsoft.EntityFrameworkCore
+        ///PM> Install-Package Microsoft.EntityFrameworkCore.SqlServer
+        ///PM> Install-Package Microsoft.EntityFrameworkCore.Tools
+        ///PM> Install-Package Microsoft.EntityFrameworkCore.InMemory
+        ///PM> Install-Package System.ConfigurationManager
+        ///PM> Install-Package Microsoft.Data.SqlClient
+        ///PM> Install-Package Dul
+
+        public SateliteDbContext()
+        {
+
+        }
+        public SateliteDbContext(DbContextOptions<SateliteDbContext> options) 
+            :base(options)
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                //string connectionString = ConfigurationManager.ConnectionStrings
+                //    ["ConnectionString"].ConnectionString;
+                string connectionString = @"Server = (localdb)\\mssqllocaldb; Database = SatelliteApp; Trusted_Connection = True;";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+        /// <summary>
+        /// 만약 날자 모델이 있으며 그값이 자동으로 들어가도록 하자 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // 테이블의 DateTime 컬럼은 자동으로 GetData() 제약 빌더를 부여하기
+            modelBuilder.Entity<SatelliteData>().Property(m => m.FileCreateDate).HasDefaultValueSql("GetDate()");
+        }
+        //[!] 현제 모든 테이블의대한참조
+        public DbSet<SatelliteData> SatelliteDatas { get; set; }
+    }
+}
--- EF_TEST/Program.cs
+++ EF_TEST/Program.cs
@@ -1,152 +1,88 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using EF_TEST;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
-//public class CameraData
-//{
-//    [Key]
-//    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-//    public int NumberID { get; set; }
-//    [StringLength(50)]
-//    public string CameraType { get; set; }
-//    [StringLength(50)]
-//    public string CameraArea { get; set; }
 
-//    public DateTime FileCreateDate { get; set; }
-//    [StringLength(255)]
-//    public string FilePath { get; set; }
-//    [StringLength(50)]
-//    public string USERS { get; set; }
-//}
-//public class VideoContext : DbContext
-//{
-//    public DbSet<CameraData> myTable { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//    {
-//        optionsBuilder.UseSqlServer("Data Source=LAPTOP-PG7BFL9M\\SQLEXPRESS01;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-//    }
 
-//    protected override void OnModelCreating(ModelBuilder modelBuilder)
-//    {
-//        modelBuilder.Entity<CameraData>().ToTable("CameraData");
-//    }
-//}
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        using (var db = new VideoContext())
-//        {
-//            db.myTable.Add(new CameraData
-//            {
-//                NumberID = 3,
-//                CameraArea = "Korea",
-//                CameraType = "IKR",
-//                FilePath = @"C\FilePath\jdfds.jpg",
-//                FileCreateDate = DateTime.Now,
-//                USERS ="PARK SANG HOON"
-//            });
-//            try
-//            {
-//                db.SaveChanges();
-//            }
-//            catch (DbUpdateException ex)
-//            {
-//                if (ex.InnerException != null && ex.InnerException.InnerException != null)
-//                {
-//                    // Log the innermost exception message
-//                    Console.WriteLine(ex.InnerException.InnerException.Message);
-//                }
-//                else
-//                {
-//                    // Log the exception message
-//                    Console.WriteLine(ex.Message);
-//                }
-//            }
-//        }
-//        Console.WriteLine("Complete");
-//        Console.ReadLine();
-//    }
-//}
-public class Customer
+public class VideoContext : DbContext
 {
-#nullable enable
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string? Address { get; set; }
-    public string? Phone { get; set; }
-#nullable disable
-    public ICollection<Order> Orders { get; set; }
-}
-
-public class Order
-{
-    public int Id { get; set; }
-    public DateTime OrderPlaced { get; set; }
-    public DateTime? OrderFulfilled { get; set; }
-    public int CustomerId { get; set; }
-
-    public Customer Customer { get; set; }
-    public ICollection<ProductOrder> ProductOrders { get; set; }
-}
-
-public class Product
-{
-    public int Id { get; set; }
-
-    [Required]
-    public string Name { get; set; }
-    [Column(TypeName = "decimal(18, 2)")]
-    public decimal Price { get; set; }
-}
-
-public class ProductOrder
-{
-    public int Id { get; set; }
-    public int Quantity { get; set; }
-    public int ProductId { get; set; }
-    public int OrderId { get; set; }
-
-    public Order Order { get; set; }
-    public Product Product { get; set; }
-}
-public class ContosoPetsContext : DbContext
-{
-    public DbSet<Customer> Customers { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<ProductOrder> ProductOrders { get; set; }
+    public DbSet<SatelliteData> myTable { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //셈플이기 때문에 여기에 connection string을 입력
-        optionsBuilder.UseSqlServer("Data Source=LAPTOP-PG7BFL9M\\SQLEXPRESS01;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SatelliteData>().ToTable("SatelliteData");
     }
 }
+
+public class MssqlDatabase2
+{
+    private DbConnection connection;
+    public void Connect()
+    {
+        connection = new SqlConnection(@"Server = (localdb)\\mssqllocaldb; Database = SatelliteApp; Trusted_Connection = True;");
+        connection.Open();
+    }
+
+    public void Disconnect()
+    {
+        connection.Close();
+    }
+
+    public DbDataReader ExcuteSql(string sql)
+    {
+        DbCommand command = new SqlCommand(sql, (SqlConnection)connection);
+        return command.ExecuteReader();
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        using ContosoPetsContext context = new ContosoPetsContext();
+        //var db2 = new MssqlDatabase2();
+        //db2.Connect();
+        //db2.ExcuteSql(@"INSERT INTO dbo.SatelliteData(NumberID, SatelliteType, SatelliteArea, FileCreateDate, FilePath, UserID)"
+        //                + @"VALUES (1, 'HD', 'Kitchen', '2020-01-01 10:00:00', 'C:\Files\image.jpg', 'John Smith')");
 
-        Product squeakyBone = new Product()
+        using (var db = new VideoContext())
         {
-            Name = "Squeaky Dog Bone",
-            Price = 4.99M
-        };
-        //레코드 추가 방법1
-        context.Products.Add(squeakyBone);
-
-        Product tennisBalls = new Product()
-        {
-            Name = "Tennis Ball 3-Pack",
-            Price = 9.99M
-        };
-        //레코드 추가 방법2
-        context.Add(tennisBalls);
-
-        context.SaveChanges();
+            db.myTable.Add(new SatelliteData
+            {
+                NumberID = 0,
+                SatelliteArea = "Korea",
+                SatelliteType = "IKR",
+                FilePath = @"C\FilePath\jdfds.jpg",
+                FileCreateDate = DateTime.Now,
+                UserID = "PARK SANG HOON"
+            });
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException != null && ex.InnerException.InnerException != null)
+                {
+                    // Log the innermost exception message
+                    Console.WriteLine(ex.InnerException.InnerException.Message);
+                }
+                else
+                {
+                    // Log the exception message
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        Console.WriteLine("Complete");
+        Console.ReadLine();
     }
 }
